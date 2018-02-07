5bdb65f273cb997461aace833fc97d2a26dc6e11
codedreamimac <codedreamfy@outlook.com>
2018-02-07 10:54:21 +0800

    init

---
+++ app.js
@@ -0,0 +1,55 @@
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const nunjucks2 = require("koa-nunjucks-2");
+
+const index = require("./routes/index");
+const users = require("./routes/users");
+
+// error handler
+onerror(app);
+
+// middlewares
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"]
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
+
+app.use(
+  nunjucks2({
+    ext: "nunjucks",
+    path: require("path").join(__dirname, "views"),
+    nunjucksConfig: {
+      trimBlocks: true,
+      lstripBlocks: true,
+      noCache: true
+    }
+  })
+);
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+
+// error-handling
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
+
+module.exports = app;
